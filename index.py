b07d6bbe99bcd3bcf662b0cb8506d3694c8f0846
alexmacharia <alex.machariam@gmail.com>
2025-09-09 15:55:51 +0300

    Added changes

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
+from sqlalchemy import create_engine
 
 df = pd.createDataFrame({'name': ['alex','maina'], 'age': [40, 41]})
 
-df.head()
+df_2 = pd.read_sql('select * from cases')
+
