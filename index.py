83d280cfca4516a4b9bf1bbcb585b36d637b9ce3
alexmacharia <alex.machariam@gmail.com>
2025-09-17 22:14:54 +0300

    Deleted test file

--- index.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from sqlalchemy import create_engine
-
-df = pd.createDataFrame({'name': ['alex','maina'], 'age': [40, 41]})
-
-df_2 = pd.read_sql('select * from cases')
-
