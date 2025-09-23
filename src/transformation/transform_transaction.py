ad890d7425e2801090066b5ca278ef13cd5de8fd
alexmacharia <alex.machariam@gmail.com>
2025-09-24 00:00:16 +0300

    Added calculate transaction metrics and detect anomalies functions

--- src/transformation/transform_transaction.py
+++ src/transformation/transform_transaction.py
@@ -81,3 +81,82 @@ class TransformTransaction:
     
         return df
     
+
+    def calculate_transaction_metrics(self, df: DataFrame) -> DataFrame:
+        """ 
+        Calculate transaction metrics
+
+        Args:
+            df (DataFrame): Dataframe with data
+
+        Returns:
+            DataFrame: Transformed dataframe with added metrics
+
+        """
+        logger.info("Calculating trnasaction metrics")
+
+        window =  Window.partitionBy("account_id").orderBy("transaction_date")
+
+        df = df.withColumn("amount_signed", 
+                           F.when(F.col("transaction_type").isin("deposit", "transfer"), F.col("amount_in_usd"))
+                           .otherwise(-F.col("amount_in_usd")))
+    
+        df = df.withColumn("running_balance", F.sum("amount_signed").over(window))
+
+        df = df.withColumn("prev_transaction_date", F.lag("transaction_date").over(window))
+
+        df = df.withColumn("days_since_last_transaction", 
+                           F.when(F.col("prev_transaction_date").isNull(), 0)
+                           .otherwise(F.datediff(F.col("transaction_date"), F.col("prev_transaction_date"))))
+    
+        window_30d = Window.partitionBy("account_id") \
+                           .orderBy(F.unix_timestamp(F.col("transaction_date"))) \
+                           .rangeBetween(-2592000, 0)  # 30 days in seconds
+    
+        df = df.withColumn("transaction_count_30d", F.count("transaction_id").over(window_30d))
+        df = df.withColumn("total_spend_30d", 
+                           F.sum(F.when(F.col("transaction_type").isin("withdrawal", "payment"), F.col("amount_in_usd"))
+                           .otherwise(0)).over(window_30d))
+    
+        return df
+    
+
+    def detect_anomalies(df: DataFrame) -> DataFrame:
+        """ 
+        Detect anomalies in transactions based on business rules
+
+        Args:
+            df (DataFrame): Transaction dataframe
+
+        Returns:
+            DataFrame: Transaction data with anomalies detected
+        """
+        logger.info("Detecting anomalies in transactions")
+
+        account_stats = df.groupBy("account_id").agg(
+            F.stddev("amount_in_usd").alias("amount_stddev"),
+            F.avg("amount_in_usd").alias("amount_avg"),
+            F.max("amount_in_usd").alias("amount_max")
+
+        )
+
+        df = df.join(account_stats, on="account_id", how="left")
+
+        df = df.withColumn("is_large_transaction", 
+                           (F.col("amount_in_usd") > (F.col("amount_avg") + 3 * F.col("amount_stddev"))) &
+                           (F.col("amount_in_usd") > 1000))
+    
+        df = df.withColumn("is_unusual_location", 
+                           F.col("is_international").cast("boolean") & 
+                           ~F.col("location").isin("Canada", "Mexico", "United Kingdom", "France", "Germany"))
+    
+        df = df.withColumn("is_high_frequency",
+                           F.col("transaction_count_30d") > 100)
+    
+        df = df.withColumn("potential_fraud", 
+                           F.col("is_large_transaction") |
+                           F.col("is_unusual_location") |
+                           (F.col("days_since_last_transaction") < 0.01))
+    
+        return df
+    
