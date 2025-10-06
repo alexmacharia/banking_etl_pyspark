17039d5ef05d311dac85bb63d81a6b325813f96b
alexmacharia <alex.machariam@gmail.com>
2025-10-06 15:18:33 +0300

    Added delta_loader function updated sparkSession configs

--- src/loading/s3_loader.py
+++ src/loading/s3_loader.py
@@ -38,6 +38,48 @@ class S3Loader:
             raise
     
 
+    def write_parquet(self, df: DataFrame, file_path: str, mode: str = "append", partition_by: List[str] = None) -> None:
+        """ 
+        Write parquet file to S3
+
+        Args:
+            df (DataFrame): Dataframe with data to write
+            file_path (str): Path to write parquet file 
+            mode (str): (append/overwrite/error)
+            partition_by (List[str]): Column(s) to partition the parquet 
+        """
+        try:
+            full_path = f"s3a://{self.bucket_name}/{file_path}"
+            logger.info(f"Writing dataframe to parquet: {full_path}")
+
+            df.write.mode(mode).partitionBy(partition_by).parquet(full_path)
+        except Exception as e:
+            logger.error(f"Error writing to parquet: {full_path}: {str(e)}")
+            raise
+
+
+        def write_delta(self, df: DataFrame, file_path: str, mode: str = "append", partition_by: List[str] = None) -> None:
+            """ 
+            Write delta files to S3
+
+            Args:
+                df (DataFrame): dataframe to write
+                file_path (str): Path to write delta files
+                mode (str): (append/overwrite/error)
+                partition_by (List[str]): Column(s) to partition the delta table
+            """
+            try:
+                full_path = f"s3a://{self.bucket_name}/{file_path}"
+                logger.info(f"Writing dataframe to delta format in path: {full_path}")
+
+                df.write.format("delta").mode(mode).partitionBy(partition_by).save(full_path)
+            except Exception as e:
+                logger.error(f"Error writing delta table to path: {full_path}: {str(e)}")
+                raise
+            
+
     
+
+        
 
 
--- src/utils/spark_session.py
+++ src/utils/spark_session.py
@@ -6,6 +6,10 @@ def create_spark_session(app_name = "Banking ETL Pipeline"):
     return (
         SparkSession.builder
                     .appName(app_name).master("local[*]")
+                    .config("spark.jars.packages", "io.delta:delta-core_2.12:2.2.0, io.delta:delta-storage_2.12:2.2.0")
+                    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension")
+                    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+                    .config("spark.databricks.delta.retentionDurationCheck.enabled", "false")
                     .config("spark.sql.adaptive.enabled", "true")
                     .config("spark.sql.adaptive.coalescePartitions.enabled", "true")
                     .getOrCreate()
