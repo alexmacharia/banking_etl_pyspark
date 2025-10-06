d31e4b9dd751ba174e294b400f6ab1f9f924c279
alexmacharia <alex.machariam@gmail.com>
2025-10-06 22:05:33 +0300

    Added write_delta_upsert function

--- src/loading/s3_loader.py
+++ src/loading/s3_loader.py
@@ -1,6 +1,7 @@
 from pyspark.sql import SparkSession, DataFrame
 import logging
 from typing import List, Optional
+from delta.tables import DeltaTable
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +19,8 @@ class S3Loader:
         self.spark = spark
         self.bucket_name = bucket_name
 
-    def write_csv(self, df: DataFrame, file_path: str, header: bool = True, mode: str = "overwrite") -> None:
+    def write_csv(self, df: DataFrame, file_path: str, header: bool = True,
+                   mode: str = "overwrite") -> None:
         """ 
         Write CSV file to S3
 
@@ -38,7 +40,8 @@ class S3Loader:
             raise
     
 
-    def write_parquet(self, df: DataFrame, file_path: str, mode: str = "append", partition_by: List[str] = None) -> None:
+    def write_parquet(self, df: DataFrame, file_path: str, mode: str = "append",
+                       partition_by: List[str] | str = None) -> None:
         """ 
         Write parquet file to S3
 
@@ -58,7 +61,8 @@ class S3Loader:
             raise
 
 
-    def write_delta(self, df: DataFrame, file_path: str, mode: str = "append", partition_by: List[str] = None) -> None:
+    def write_delta(self, df: DataFrame, file_path: str, mode: str = "append",
+                     partition_by: List[str] | str = None) -> None:
         """ 
         Write delta files to S3
 
@@ -77,6 +81,42 @@ class S3Loader:
             logger.error(f"Error writing delta table to path: {full_path}: {str(e)}")
             raise
 
+    
+    def write_delta_upsert(self, df: DataFrame, file_path: str, key_columns: List[str] = None) -> None:
+        """
+        Loading data to delta table through upsert
+
+        Args:
+            df (DataFrame): Dataframe with data to upsert
+            flie_path (str): Path to the delta table
+            key_columns (List[str]): Primary key column for merging
+        """
+        try:
+            # full_path = f"s3a://{bucket_name}/{file_path}"
+            full_path = file_path
+            logger.info(f"Writing dataframe to delta location: {full_path}")
+
+            if key_columns and len(key_columns) > 0:
+                if len(key_columns) == 1:
+                    key_conditions = f"target.{key_columns[0]} = source.{key_columns[0]}"
+                else:
+                    key_conditions = " AND ".join(f"target.{col} = source.{col}" for col in key_columns)
+                non_key_columns = [col for col in df.columns if col not in key_columns]
+                update_dict = {f"target.{col}": f"source.{col}" for col in non_key_columns}
+                insert_dict = {f"target.{col}": f"source.{col}" for col in df.columns}
+
+                delta_table = DeltaTable.forPath(self.spark, full_path)
+
+                delta_table.alias("target") \
+                    .merge(df.alias("source"), key_conditions) \
+                    .whenMatchedUpdate(set = update_dict) \
+                    .whenNotMatchedInsert(values = insert_dict) \
+                    .execute()
+        
+            logger.info(f"Successfully loaded data to delta table {full_path}")    
+        except Exception as e:
+            logger.error(f"Error upserting to delta table path {full_path}: {str(e)}")
+            raise
 
     
 
