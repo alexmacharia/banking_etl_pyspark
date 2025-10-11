baab6c726ddbba4475bfa428276624d716fb5877
alexmacharia <alex.machariam@gmail.com>
2025-10-11 20:24:44 +0300

    Added enrich_customer_data to transform_customer

--- src/transformation/transform_customer.py
+++ src/transformation/transform_customer.py
@@ -31,6 +31,8 @@ class CustomerTransformer:
             DataFrame: Cleaned customers data
     
         """
+        logger.info("Cleaning customer data")
+
         # Convert date strings to date format
         df = df.withColumn("date_of_birth", F.to_date("date_of_birth"))
 
@@ -41,5 +43,36 @@ class CustomerTransformer:
 
         # Handle missing values 
         df = df.fillna("Unknown", ["city", "state", "country", "zip_code", "risk_segment"])
+
+        return df
+    
+
+    def enrich_customer_data(self, df: DataFrame) -> DataFrame:
+        """ 
+        Enrich customer data
+
+        Args:
+            df (DataFrame): Raw customer data
+
+        Returns:
+            DataFrame: Enrcihed customer dataframe
+        """
+        logger.info("Enriching customer data")
+    
+        # Get full name
+        df = df.withColumn("full_name", F.concat("first_name", F.lit(" "), "last_name"))
+ 
+        # Combine address info to get full address
+        df = df.withColumn("full_address", F.concat_ws(", ", "address", "city", "state", "zip_code", "country"))
+
+        # Calculate the age
+        df = df.withColumn("age", F.round(F.datediff(F.current_date(), "date_of_birth") / 365, 0).cast("int"))
+
+        # Derive age group from age
+        df = df.withColumn("age_group", F.when(F.col("age").between(18, 35), "18 to 35")
+                                         .when(F.col("age").between(36, 50), "36 to 50")
+                                         .when(F.col("age").between(51, 65), "51 to 65")
+                                         .when(F.col("age") >= 66, "Over 66")
+                                         .otherwise("underage") )
 
         return df
